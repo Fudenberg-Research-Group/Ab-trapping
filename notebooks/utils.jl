156a1f532aa246c82fd78c2434c6661fc5ac6832
Miles <xingchi_yan@brown.edu>
2025-04-03 13:33:15 -0700

    Moving core simulations into utils

--- notebooks/utils.jl
+++ notebooks/utils.jl
@@ -2,6 +2,96 @@
 
 # utils.jl
 
+## Simulation 
+
+function radial_laplacian_3d_Neumann(N_r, r_max)
+    rd = range(0, stop=r_max, length=N_r)
+    dr = step(rd)
+    ir_indices = Int[]
+    jc_indices = Int[]
+    data_values = Float64[]
+
+    for ir in 1:N_r
+        r = rd[ir]
+        center_idx = ir
+        center_val = 0.0
+
+        # Neumann boundary condition at r = 0 (ir == 1)
+        if ir == 1
+            push!(ir_indices, center_idx)
+            push!(jc_indices, center_idx)
+            push!(data_values, -1/(dr^2))   # Center contribution
+ 
+            if N_r > 1
+                push!(ir_indices, center_idx)
+                push!(jc_indices, center_idx + 1)  # Right neighbor
+                push!(data_values, 1/(dr^2))       # Contribution from T_1
+            end
+            
+        # Internal points
+        elseif ir > 1 && ir < N_r
+            if ir > 1  # Left neighbor
+                push!(ir_indices, center_idx)
+                push!(jc_indices, ir - 1)
+                push!(data_values, 1/(dr^2) - 1/(dr * r))  # Radial contribution
+            end 
+
+            if ir < N_r  # Right neighbor
+                push!(ir_indices, center_idx)
+                push!(jc_indices, ir + 1)
+                push!(data_values, 1/(dr^2) + 1/(dr * r))  # Radial contribution
+            end
+            
+            # Center position contribution for internal points
+            center_val -= 2/(dr^2)  # Total contribution from neighbors
+            push!(ir_indices, center_idx)
+            push!(jc_indices, center_idx)
+            push!(data_values, center_val)
+        end
+        
+        # Neumann boundary condition at r = r_max (last point)
+        if ir == N_r
+            push!(ir_indices, center_idx)
+            push!(jc_indices, center_idx)
+            push!(data_values, -1/(dr^2))  # Center contribution
+
+            if N_r > 1
+                push!(ir_indices, center_idx)
+                push!(jc_indices, center_idx - 1)  # Left neighbor
+                push!(data_values, 1/(dr^2))  # Contribution from T_{N-1}
+            end
+        end
+    end 
+    
+    return sparse(ir_indices, jc_indices, data_values, N_r, N_r)
+end
+
+function init_concentration_3D_spherical_smoothed(rgrid, epsilon, u1_const, u2_const, R0)
+    # rgrid is a vector of radial distances
+    N_r = length(rgrid)
+    # Initialize the concentration field with zeros
+    u = zeros(N_r, 3)  # Assuming u has three components (u1, u2, u3)
+    for i in 1:N_r
+        r = rgrid[i]
+        u[i, 1] = u1_const/2 * (1 + tanh((r^2 - R0^2) / epsilon))  # Smooth transition for u1
+        u[i, 2] = u2_const/2 * (1 - tanh((r^2 - R0^2) / epsilon))  # Smooth transition for u2
+        u[i, 3] = 0  # u3 remains zero
+    end
+    u
+end
+
+# Define the ODE function for spherical diffusion
+function diffuse_spherical_diff(du, u, p, t)
+    D, kon, koff = p
+    @inbounds du[:, 1] = D * mat_div * u[:, 1] + koff * u[:, 3] - kon * u[:, 1].*u[:, 2]
+    @inbounds du[:, 2] = + koff * u[:, 3] - kon * u[:, 1].*u[:, 2]
+    @inbounds du[:, 3] = - koff * u[:, 3] + kon * u[:, 1].*u[:, 2]
+end
+
+
+## Visualization  
+
+
 function extract_data(u_data::Vector{String}, time, indicator)
     # Step 1: Remove the square brackets and split the string by semicolons into rows
     rows = split(strip(u_data[time], ['[', ']']), ";")
